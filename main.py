eda6e6e4d40562dee1191d9eaa4bb8f0c02f70b3
pippy360 <tomnomnom1@gmail.com>
2016-09-22 17:01:23 +0100

    backing up before going home

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def start(path='./data'):
 
 
 	org_test_images, org_test_labels = load_wrapper(path, test_img_fname, test_lbl_fname)	
-	org_test_images, org_test_labels = mixThem_random(org_test_images, org_test_labels, validInts=[5,6,7,8,9])
+	org_test_images, org_test_labels = mixThem_random(org_test_images, org_test_labels, validInts=[0,1,2,3,4,5,6,7,8,9])
 	fin_images = packOurNewData(org_test_labels, org_test_images)
 	#print fin_images
 	text_file = open("test_mixed_numbers_data.csv", "w+")
@@ -153,7 +153,7 @@ def packImages(images, labels, width=28, magic_num=2051):
 	size = len(images)
 	cols = width
 	rows = len(images[0])/cols
-	lengthOfTheWholeThing=3000
+	lengthOfTheWholeThing=6000
 	packed = ""
 	topCount = 1568#len(images[0])
 	packed = packed + str(lengthOfTheWholeThing) + ","

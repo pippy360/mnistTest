f2da62dfa9baa408ff949a98237e1e6b196fe957
pippy360 <tomnomnom1@gmail.com>
2016-09-30 16:12:14 +0100

    still working on it

---
+++ display_train.py
@@ -0,0 +1,52 @@
+import numpy as np
+from numpy import genfromtxt
+from random import randint
+import array_vals as av
+
+
+def main():
+	lines = loadTheData("train_mixed_numbers_data.csv")
+	
+	
+	for i in range(99):
+		line, label = getARandomLine(lines)
+		print "label :" + str(label)
+		print displayValue(line, label)
+
+def loadTheData(path):
+	lines = genfromtxt(path, delimiter=',', skip_header=1, max_rows=100)
+	return lines
+
+def getARandomLine(lines):
+	lineIdx = randint(0,len(lines)-1)
+	line = lines[lineIdx]
+	
+	#format it as a list
+	retLine = []	
+	for i in range(len(line)):
+		retLine.append(line[i])
+	line = retLine
+
+	#get the label
+	boolVal = line[-1]
+	line.pop()
+
+	return line, boolVal 
+
+
+def displayValue(wholeLineAsArray, label, cols=28):
+	rows = len(wholeLineAsArray)/cols
+	ret = ""
+	tempStr = "TRUE. Match found" if label else "FALSE. No match found"
+	ret = ret + tempStr
+	for i in range(rows):
+		for j in range(cols):
+			idx = (i*cols) + j
+			#ret = ret + str(wholeLineAsArray[idx])
+			value = '@' if wholeLineAsArray[idx] > 200 else ' '
+			ret = ret + value
+
+		ret = ret + "\n"
+	return ret
+
+main()
